9f75295a40c7052fda82a6d6a0a7a15da7bcf694
Tejal Vegad <tejal@bongolive.co.tz>
2021-06-09 10:39:55 +0300

    add scripts for redirct method

---
+++ redirect/node-js/redirect.js
@@ -0,0 +1,40 @@
+const axios = require('axios');
+const url = 'https://checkout.beem.africa/v1/checkout';
+const https = require('https');
+const btoa = require('btoa');
+
+const api_key = '<api_key>';
+const secret_key = '<secret_key>';
+
+const payload = {
+    amount: '<amount>',
+    reference_number: '<reference_number>',
+    mobile: '<mobile>',
+    sendSource: '<sendSource>',
+   };
+
+
+function getCheckout() {
+   axios
+   .get(url,payload,{
+      headers: {
+        'Content-Type': 'content_type',
+       Authorization: 'Basic ' + btoa(api_key + ':' + secret_key),
+     },
+      httpsAgent: new https.Agent({
+       rejectUnauthorized: false,
+      }),
+   })
+    .then((res) => {
+    // sendSource as true 
+    if (res.request.responseURL) {
+        window.location.href = res.request.responseURL;
+       }
+    //sendSource as false
+    // console.log(response.data));    
+    })
+    .catch((error) => console.error(error.response.data))
+
+}
+
+getCheckout();
---
+++ redirect/php/redirect.php
@@ -0,0 +1,39 @@
+<?php
+$username='<api_key>';
+$password = '<secret_key>';
+
+$Url ='https://checkout.beem.africa/v1/checkout';
+
+ 
+ $reference_number='<reference_number>'; 
+ $amount ='<amount>'; 
+ $mobile ='<mobile>'; 
+ $sendSource ='<sendSource>'; 
+ $body = array('amount'=>$amount,'reference_number'=>$reference_number,'mobile'=>$mobile,'reference_number'=>$reference_number);
+
+// Setup cURL
+$ch = curl_init($Url);
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt_array($ch, array(
+    CURLOPT_HTTPGET => TRUE,
+    CURLOPT_RETURNTRANSFER => TRUE,
+    CURLOPT_HTTPHEADER => array(
+        'Authorization:Basic ' . base64_encode("$username:$password"),
+        'Content-Type: application/json'
+    ),
+));
+
+// Send the request
+$response = curl_exec($ch);
+
+// Check for errors
+if($response === FALSE){
+        echo $response;
+
+    die(curl_error($ch));
+}
+var_dump($response);
+?>
