5606983b384e147d52c492c196c1df79d93d8631
Tejal Vegad <tejal@bongolive.co.tz>
2021-07-15 14:40:25 +0300

    fix: sample codes for payment-checkout

--- redirect/node-js/redirect.js
+++ redirect/node-js/redirect.js
@@ -1,41 +1,43 @@
-const axios = require('axios');
-const url = 'https://checkout.beem.africa/v1/checkout';
-const https = require('https');
-const btoa = require('btoa');
+const axios = require("axios");
+const https = require("https");
+const btoa = require("btoa");
 
-const api_key = '<api_key>';
-const secret_key = '<secret_key>';
+const url = "https://checkout.beem.africa/v1/checkout";
+const api_key = "<API_KEY>";
+const secret_key = "<SECRET_KEY>";
+const content_type = "application/json";
 
-const payload = {
-    amount: '<amount>',
-    reference_number: '<reference_number>',
-    mobile: '<mobile>',
-    sendSource: '<sendSource>',
-    transaction_id: '<transaction_id>',
-   };
-
+let payload = {
+  headers: {
+    "Content-Type": content_type,
+    Authorization: "Basic " + btoa(api_key + ":" + secret_key),
+  },
+  params: {
+    amount: "<amount>",
+    reference_number: "<reference_number>",
+    mobile: "<mobile>",
+    sendSource: "<sendSource>",
+    transaction_id: "<transaction_id>",
+  },
+};
 
 function getCheckout() {
-   axios
-   .get(url,payload,{
-      headers: {
-        'Content-Type': 'content_type',
-       Authorization: 'Basic ' + btoa(api_key + ':' + secret_key),
-     },
+  axios
+    .get(url, payload, {
       httpsAgent: new https.Agent({
-       rejectUnauthorized: false,
+        rejectUnauthorized: false,
       }),
-   })
-    .then((res) => {
-    // sendSource as true 
-    if (res.request.responseURL) {
-        window.location.href = res.request.responseURL;
-       }
-    //sendSource as false
-    // console.log(response.data));    
     })
-    .catch((error) => console.error(error.response.data))
-
+    .then((res) => {
+      console.log(res);
+      // sendSource as true
+      if (res.request.responseURL) {
+        window.location.href = res.request.responseURL;
+      }
+      //sendSource as false
+      console.log(res.data);
+    })
+    .catch((error) => console.error(error.response.data));
 }
 
 getCheckout();
--- redirect/php/redirect.php
+++ redirect/php/redirect.php
@@ -1,29 +1,32 @@
 <?php
-$username='<api_key>';
+$username = '<api_key>';
 $password = '<secret_key>';
 
-$Url ='https://checkout.beem.africa/v1/checkout';
+$Url = 'https://checkout.beem.africa/v1/checkout';
 
- 
- $reference_number='<reference_number>'; 
- $amount ='<amount>'; 
- $mobile ='<mobile>'; 
- $transaction_id ='<transaction_id>'; 
- $sendSource ='<sendSource>'; 
- $body = array('amount'=>$amount,'transaction_id'=>$transaction_id,'reference_number'=>$reference_number,'mobile'=>$mobile,'sendSource'=>$sendSource);
+$reference_number = '<reference_number>';
+$amount = '<amount>';
+$mobile = '<mobile>';
+$transaction_id = '<transaction_id>';
+$sendSource = '<sendSource>';
+$body = array('amount' => $amount, 'transaction_id' => $transaction_id, 'reference_number' => $reference_number, 'mobile' => $mobile, 'sendSource' => $sendSource);
 
 // Setup cURL
-$ch = curl_init($Url);
+$ch = curl_init();
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+
+$Url = $Url . '?' . http_build_query($body);
+
+curl_setopt($ch, CURLOPT_URL, $Url);
 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 curl_setopt_array($ch, array(
-    CURLOPT_HTTPGET => TRUE,
-    CURLOPT_RETURNTRANSFER => TRUE,
+    CURLOPT_HTTPGET => true,
+    CURLOPT_RETURNTRANSFER => true,
     CURLOPT_HTTPHEADER => array(
         'Authorization:Basic ' . base64_encode("$username:$password"),
-        'Content-Type: application/json'
+        'Content-Type: application/json',
     ),
 ));
 
@@ -31,10 +34,9 @@ curl_setopt_array($ch, array(
 $response = curl_exec($ch);
 
 // Check for errors
-if($response === FALSE){
-        echo $response;
+if ($response === false) {
+    echo $response;
 
     die(curl_error($ch));
 }
 var_dump($response);
-?>
